23c77e1c4c1c3c60845f22cdc8742e90a3dd1548
thenameisdrey <dare.sunday@outlook.com>
2022-02-06 23:23:52 +0100

    add counter project

---
+++ 15_Javascript_Project/02-counter/setup/app.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+const btns = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click",function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {value.style.color = "green";}
+        if (count < 0) {value.style.color = "red";}
+        if (count ==  0) {value.style.color = "#222";}
+        value.textContent = count;
+    });
+});
