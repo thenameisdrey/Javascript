f81f9e760d52fdf3be0242a4246b9fa5c2102a28
thenameisdrey <dare.sunday@outlook.com>
2022-02-23 12:20:53 +0100

    m

--- 15_Javascript_Project/08-menu/setup/app.js
+++ 15_Javascript_Project/08-menu/setup/app.js
@@ -73,49 +73,28 @@ const menu = [
   },
 ];
 
-const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn')
+const sectionCenter = document.querySelector(".section-center");
 
-filterBtns.forEach(function(btns){
-  btns.addEventListener('click',function(e){
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem){
-
-      if (menuItem.category === category){
-        return menuItem;
-      }      
-    });
-    if (category === "all") {
-      displayMenuItems(menu);
-    } else {
-      displayMenuItems(menuCategory);
-    }
-  });
+window.addEventListener('DOMContentLoaded',function () {
+  displayMenuItems(menu);
 });
 
-
-window.addEventListener('DOMContentLoaded',function (){
-  displayMenuItems(menu);
-})
-
-function displayMenuItems(menuItems){
-  let displayMenu = menuItems.map(function (item) {
-    //console.log(item);
-
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function(item){
     return ` <article class="menu-item">
-        <img src=${item.img} alt=${item.img} class="photo" />
-        <div class="item-info">
-          <header>
-            <h4>${item.title}</h4>
-            <h4 class="price">${item.price}</h4>
-          </header>
-          <p class="item-text">
-            ${item.desc}
-          </p>
-        </div>
-      </article>
-    `;
+    <img src=${item.img} alt=${item.img} class="photo" />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>
+`;
   });
   displayMenu = displayMenu.join('');
-  sectionCenter.innerHTML = displayMenu
+  sectionCenter.innerHTML = displayMenu;
 }
