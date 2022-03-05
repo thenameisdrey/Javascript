5313b203c15eb752001e500a497ee71ce84c3ecf
thenameisdrey <dare.sunday@outlook.com>
2022-03-05 15:20:18 +0100

    modern web on React

---
+++ React/api_tuts/src/containers/features/Features.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './features.css'
+
+const Features = ({ title, text }) => {
+  return (
+    <div className='gpt3__features-container__feature'>
+      <div className="gpt3__features-contaner__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        {text}
+      </div>
+
+    </div>
+  )
+}
+
+export default Features
